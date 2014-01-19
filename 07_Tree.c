2068fa35d4fd72a05e39eee75e3e78ac03efd2c4
susan <eehanwenfang@foxmail.com>
2014-01-19 20:23:54 +0800

    properties of tree

---
+++ 07_Tree.c
@@ -0,0 +1,43 @@
+/****************************************************
+ * Index Methods ==> 
+ *	Parents Pattern  -- O(1)[Get parent ]
+ *	Children Pattern
+ *	Pattern & Children Pattern
+ *	Children and rightBrother Pattern
+ *	Binary Tree Pattern
+
+parent pattern and children pattern are reversed.
+
+ * Binary Tree
+	* preorder 
+	* inorder
+	* postorder
+	* level order[FIFO queue]
+
+ * Some IDs
+ 	* depth: k
+ 	* max num of tree nodes: 2^k -1
+ 	* max num of kth level tree nodes: 2^(k-1)
+ 	* node i --> lower_bound(i/2)
+ 			 --> left child 2*i (2*i<n)
+ 			 --> right child 2*i+1 (2*i+1 < n)
+*****************************************************/
+
+
+
+
+int main(int argc, char *argv[])
+{
+	printf("test!\n");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
